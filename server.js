a4571455d271f631fdb9dabc565f9d77d1062f3f
Dipta <715diptamondal@gmail.com>
2025-12-07 12:40:42 +0530

    Initial commit for online card game server

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Client connected:", socket.id);
+
+    // When a player joins a room
+    socket.on("joinRoom", (roomId) => {
+        socket.join(roomId);
+        console.log(`Player ${socket.id} joined room ${roomId}`);
+
+        io.to(roomId).emit("playerJoined", {
+            playerId: socket.id,
+            roomId: roomId
+        });
+    });
+
+    // When a player plays a card
+    socket.on("playCard", (data) => {
+        console.log(`Card played in ${data.roomId}:`, data.card);
+
+        // Broadcast to everyone in the room
+        io.to(data.roomId).emit("cardPlayed", {
+            player: socket.id,
+            card: data.card
+        });
+    });
+
+    // When user disconnects
+    socket.on("disconnect", () => {
+        console.log("Player disconnected:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});
